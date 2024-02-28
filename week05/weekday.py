c64184b3b504f01f9013680af2d8bc9e224a2e59
Jenny Ibanez <jenny.ibanezcamo@gmail.com>
2024-02-28 21:10:32 +0000

    weekdays

---
+++ week05/weekday.py
@@ -0,0 +1,16 @@
+# This program will outputs whether or not today is a weekday.
+
+# Author Jennifer Ibanez Cano 
+
+# I'll import the datetime package. Python’s built-in datetime library 
+# is one of the most common modules to manipulate date and time object data. 
+from datetime import datetime
+
+# datetime.today stores the today's date 
+# .weekday() will return an integer with the day of the week:
+# 0 = Mon, 1 = Tue, 2 = Wed, 3 = Thur, 4 = Fri, 5 = Sat, 6 = Sun
+if datetime.today().weekday() == 5 or datetime.today().weekday() == 6:
+    print("It is the weekend, yay!")
+
+else:
+    print("Yes, unfortunately today is a weekday")
